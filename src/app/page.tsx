061893a6352a2bea988f081dbb032bc506f20903
Vinay Vishwakarma <vinayvishwakarma12342004@gmail.com>
2024-11-21 11:42:20 +0530

    Created a Card Component

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ThreeDCardDemo } from "@/components/ui/ThreeDCardDemo";
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="mx-28 bg-slate-100 ">
+      <ThreeDCardDemo />
+    </div>
+  );
+}
